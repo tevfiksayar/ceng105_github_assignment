a5fc129c0fcca66280f6b6ff616da3a365cd829b
tevfiksayar <c2211059@student.cankaya.edu.tr>
2023-11-18 21:34:14 +0300

    toplama işlemi eklendi

---
+++ cmake-build-debug/ceng105_github_assigment.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+
+    int sayi1, sayi2, toplam, cikar, carp, bol;
+
+    printf("Lütfen birinci sayıyı giriniz: ");
+    scanf(" %d", &sayi1);
+
+    printf("Lütfen ikinci sayıyı giriniz: ");
+    scanf(" %d", &sayi2);
+
+    toplam = sayi1 + sayi2;
+
+
+    printf("sayi1 + sayi2: %d\n", toplam);
+
+
+
+    return 0;
+}
